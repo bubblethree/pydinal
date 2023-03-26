8c285e9cf19c7b362d37bd7fa902cae6bdd84ab5
bubblethree <frederik@flinhardt.de>
2023-03-26 18:20:53 +0200

    changed variable names

--- pydinal/inf_ordinal.py
+++ pydinal/inf_ordinal.py
@@ -2,7 +2,7 @@
 #this is the class for infinite ordinal below epsilon_0
 class InfOrdinal():
     
-    def __init__(self, exp_ord=1, mult_ord =1, add_ord =0):
+    def __init__(self, exp_ord=1, coe =1, add_ord =0):
 
         """every ordinal alpha that is smaller than epsilon_0 can be expressed in the following way: 
         alpha=w^beta * n + gamma, where beta, gamma are ordinals smaller than epsilon_0 and n is a natural number
@@ -11,8 +11,8 @@ class InfOrdinal():
         if not isinstance(exp_ord,(InfOrdinal,int)):
             raise TypeError("Wrong data type for exponent, should be int or an InfOrdinal but is: "+str(type(exp_ord)) )
 
-        if not isinstance(mult_ord,(InfOrdinal,int)):
-            raise TypeError("Wrong data type for multiplicator, should be int or an InfOrdinal but is: "+str(type(mult_ord)) )
+        if not isinstance(coe,(int)):
+            raise TypeError("Wrong data type for coefficient, should be an int but is: "+str(type(coe)) )
 
         if not isinstance(add_ord,(InfOrdinal,int)):
             raise TypeError("Wrong data type for added ordinal, should be int or an InfOrdinal but is: "+str(type(add_ord)) )
@@ -20,7 +20,7 @@ class InfOrdinal():
         if not (exp_ord > 0 ):
             raise ValueError("exponent has to be larger than 0" )
         
-        if (mult_ord <= 0 ):
+        if (coe <= 0 ):
             raise ValueError("multiplicator has to be larger than 0" )
 
         if (add_ord < 0 ):
@@ -28,18 +28,18 @@ class InfOrdinal():
         
         self.exp_ord=exp_ord
 
-        self.mult_ord=mult_ord
+        self.coe=coe
 
         self.add_ord=add_ord
     
     #returns a copy of the ordinal
     def copy(self):
-        return InfOrdinal(self.exp_ord,self.mult_ord,self.add_ord)
+        return InfOrdinal(self.exp_ord,self.coe,self.add_ord)
     
     #This will be useful when comparing the ordinals
     def to_list(self):
 
-        return [self.exp_ord,self.mult_ord, self.add_ord]
+        return [self.exp_ord,self.coe, self.add_ord]
     
     #an ordinal < e_0 is exactly a limit ordinal when it has no finite part
     def limit_ord(self):
@@ -107,7 +107,7 @@ class InfOrdinal():
             return True
         return self.to_list() > other.to_list()
 
-    #return the len of the ordinal i.e the amount of mult_ord that != 0
+    #return the len of the ordinal i.e the amount of coe that != 0
     def __len__(self):
         counter=0
         working_ordinal=self
@@ -127,7 +127,7 @@ class InfOrdinal():
             if other <0:
                 raise TypeError("Can not add ordinal to "+ str(type(other)))
             
-            return InfOrdinal(self.exp_ord, self.mult_ord, self.add_ord+other)
+            return InfOrdinal(self.exp_ord, self.coe, self.add_ord+other)
         
         '''w**(a_n)*b_n+..+ w**(a_1)*b_1 + b_0 + w**(c)*d
         = w**(c_n)*d_n+..+ w**(c_i)*d_1 + d_0  if c > a_n
@@ -138,17 +138,17 @@ class InfOrdinal():
         if (self.add_ord==0):
 
             if (self.exp_ord > other.exp_ord):
-                return InfOrdinal(self.exp_ord, self.mult_ord, self.add_ord+other)
+                return InfOrdinal(self.exp_ord, self.coe, self.add_ord+other)
 
             if (self.exp_ord == other.exp_ord):
-                return InfOrdinal(self.exp_ord, self.mult_ord+other.mult_ord, self.add_ord)
+                return InfOrdinal(self.exp_ord, self.coe+other.coe, self.add_ord)
 
             if (self.exp_ord < other.exp_ord):
                 return other
             
         #This just splits up the add_ordinal in it's w components
         else:
-            return InfOrdinal(self.exp_ord, self.mult_ord, 0) + (self.add_ord+other)
+            return InfOrdinal(self.exp_ord, self.coe, 0) + (self.add_ord+other)
 
     
     
@@ -165,7 +165,7 @@ class InfOrdinal():
     def __mul__(self,other):
         
         if not isinstance(other, (InfOrdinal,int)):
-            raise NotImplemented
+            raise TypeError("Can not add multiply with "+str(type(other)))
 
         if isinstance(other,int):
             
@@ -174,7 +174,7 @@ class InfOrdinal():
             
             #(w**(a_n)*b_n+..+ w**(a_1)*b_1 + b_0)*m)=w**(a_n * m)*b_n+..+ w**(a_1)*b_1 + b_0
             else:
-                return InfOrdinal(self.exp_ord,self.mult_ord*other,self.add_ord)
+                return InfOrdinal(self.exp_ord,self.coe*other,self.add_ord)
 
         else:
             '''
@@ -184,8 +184,8 @@ class InfOrdinal():
             '''
             return (
                 InfOrdinal(self.exp_ord+other.exp_ord,  
-                        other.mult_ord,
-                        InfOrdinal(self.exp_ord,self.mult_ord,self.add_ord)*other.add_ord)
+                        other.coe,
+                        InfOrdinal(self.exp_ord,self.coe,self.add_ord)*other.add_ord)
             )
 
     def __rmul__(self, other):
@@ -203,7 +203,7 @@ class InfOrdinal():
     def __pow__(self,other):
         
         if not isinstance(other, (int,InfOrdinal)):
-            raise NotImplemented
+            raise ValueError("ordinal can be raised to the power of "+str(type(other)))
         
         if other<0:
             raise ValueError("Exponent has to be larger than 0")
@@ -232,35 +232,35 @@ class InfOrdinal():
     def __rpow__(self, other):
         
         if not isinstance(other, (int,InfOrdinal)):
-            raise NotImplemented
+            raise ValueError("ordinal exponent not defined for "+str(type(other)))
         
         if other <0:
             raise ValueError("can not raise a negative number to the power of an ordinal")
         
         #k**(w*a+b) = k**(w*a) * k**(b)= (k**w)**a + k**(b)=w**a + k**(b)
         if self.exp_ord==1:
-            return InfOrdinal(self.mult_ord, other**(self.add_ord), 0)
+            return InfOrdinal(self.coe, other**(self.add_ord), 0)
         
         #k**(w**(a)*b + n) = w**(w**(a-1) * b) * (n ** k)
         if isinstance(self.add_ord,int):
 
             if isinstance(self.exp_ord, int):
-                help_ord=InfOrdinal(max(self.exp_ord-1,0),self.mult_ord,0)
+                help_ord=InfOrdinal(max(self.exp_ord-1,0),self.coe,0)
             
             else:
-                help_ord=InfOrdinal(self.exp_ord.pred(),self.mult_ord,0)
+                help_ord=InfOrdinal(self.exp_ord.pred(),self.coe,0)
             return InfOrdinal(help_ord, other**(self.add_ord),0)
         
         #k**(w**(a_n)*b_n + .. + w**(a_1)*b_1 + b_0) =
         #k**(w**(a_n)*b_n * k**(w**(a_(n-1))*b_(n-1) + .. + w**(a_1)*b_1 + b_0)
         else:
-            return other**InfOrdinal(self.exp_ord, (self.mult_ord),0) * other**(self.add_ord)
+            return other**InfOrdinal(self.exp_ord, (self.coe),0) * other**(self.add_ord)
     
 
     
 
-    '''gives the ordinal back as a string of the form: "w**(exp_ord)*mult_ord + add_ord"
-    ommitts the exponent and mult_ord if they are 1 and the add_ord when it is 0
+    '''gives the ordinal back as a string of the form: "w**(exp_ord)*coe + add_ord"
+    ommitts the exponent and coe if they are 1 and the add_ord when it is 0
     '''
     def __str__(self):
     
@@ -268,8 +268,8 @@ class InfOrdinal():
         if (self.exp_ord>1):
             ordinal_string+="**("+str(self.exp_ord)+")"
 
-        if (self.mult_ord>1):
-            ordinal_string+="*"+str(self.mult_ord)
+        if (self.coe>1):
+            ordinal_string+="*"+str(self.coe)
 
         if (self.add_ord>0):
             ordinal_string+=" + "+str(self.add_ord)
